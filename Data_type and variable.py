7835617fb37b781edf44b0384e12854b0a2fad25
Deepak-18449 <deepakaswani2004@gmail.com>
2025-01-04 09:47:35 +0530

    Data_type

---
+++ Data_type and variable.py
@@ -0,0 +1,38 @@
+# what are data type in python?
+'''
+Data type specifies the type of value a variable requires to do various operations without causing an error. By default, python provides the following built-in data types
+'''
+# what is variable in python ?
+'''
+In python a variable is a named location in computer memory that stores a value and is used throughout the code
+'''
+# example of variable 
+a=10 
+b=20
+print(a+b)
+'''
+so a and b vairable in example
+'''
+# How many data type in  python ?
+'''
+There are five typw of data type in python 
+Integers
+Floating point numbers
+String
+Booeans
+None
+'''
+a=10 #a is an integers
+b=10.5 #b is an floating point number
+c="harry"  #c is an string
+d=True #d is an booeans
+e=None #e is an none 
+# write a python program add two numbers
+num1 = 20
+num2 = 40
+print(num1+num2)
+print(num2%num1)
+z=int(input("enter a number4"))
+print(num2>num1)
+print(num1+num2%2)
+print(num1**2)
